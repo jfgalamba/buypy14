5c955560283e57d6f17cb42e7a7f6f54626e92c3
jgalamba <jfgalamba@mail>
2022-08-08 15:00:35 +0100

    Initial commit.

---
+++ src/db/buypy.sql
@@ -0,0 +1,16 @@
+/*
+    DDL for the BuyPy Online Store.
+
+    (c) Joao Galamba, 2022
+ */
+
+DELIMITER //
+
+DROP DATABASE IF NOT EXISTS BuyPy
+//
+CREATE DATABASE BuyPy
+//
+
+USE BuyPy
+
+
---
+++ src/db/buypy_data.sql
@@ -0,0 +1,10 @@
+/*
+    DML for the BuyPy Online Store.
+
+    (c) Joao Galamba, 2022
+ */
+
+USE BuyPy
+
+
+
